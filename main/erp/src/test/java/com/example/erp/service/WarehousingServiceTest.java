feab9ce81c6f8d84597120b7f4c9dfa9c28aa511
DESKTOP-HUDI05M\shin0 <shin0586@naver.com>
2024-04-13 18:52:25 +0900

    test 작성

--- main/erp/src/test/java/com/example/erp/service/WarehousingServiceTest.java
+++ main/erp/src/test/java/com/example/erp/service/WarehousingServiceTest.java
@@ -1,5 +1,12 @@
 package com.example.erp.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
+@SpringBootTest
+@Transactional
 public class WarehousingServiceTest {
+
 }
